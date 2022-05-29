4906f0bbefeb1fbb4e4645be91e08dec29d3040a
MaititeMohamed <maititemohamed@gmail.com>
2022-05-29 06:59:25 -1200

    creat navbar file and create navbar withe inc in the auther files

---
+++ app/views/inc/navbar.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+   <img src="<?php echo URLROOT; ?>/public/img/fixeto.png"/>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <i class="fa-solid fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="<?php echo URLROOT; ?>">Home</a>
+        </li>
+
+        <li class="nav-item">
+        <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+       
+       
+      </ul>
+     
+    </div>
+  </div>
+</nav>
