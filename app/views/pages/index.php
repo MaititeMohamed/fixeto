a2f8cd8743c1888617a2933a9ba3605a8512a758
MaititeMohamed <maititemohamed@gmail.com>
2022-05-31 02:41:09 -1200

    add pholder  OUR SERVICES img

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -48,8 +48,8 @@
 <!-- slide start  -->
 
 <!-- start about Us -->
-<section class="container">
-  <div class="row pt-5  ">
+<section class="container p-5 ">
+  <div class="row   ">
     <div class="col-md-12  col-lg-6 ">
       <div>
         <img src="<?php echo URLROOT; ?>/public/img/All/image_01.jpg" class="d-block w-100 imgh " alt="...">
@@ -58,22 +58,49 @@
 
     </div>
 
-    <div class="col-md-12  col-lg-6 about  ">
-      <div class="mt-4">
+    <div class="col-md-12  col-lg-6 about d-flex align-items-center   ">
+      <div class="mt-4   ">
         <h3>Fixeto</h3>
-        <h1 class="test">About Us</h1>
-        <p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tincidunt velit mauris. Integer
+        <h1 class=" text-primary">About Us</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tincidunt velit mauris. Integer
           mollis tellus blandit, luctus lorem quis, lacinia magna. Nam ut volutpat ipsum, at scelerisque
           tellus. Vivamus mattis est libero,
           Sed hendrerit, odio sed congue consectetur, felis mi finibus enim, in egestas justo lacus sed sapien.
           In pharetra sollicitudin leo, molestie molestie sapien lacinia sed. Suspendisse sit amet purus sit
-        
+
         </p>
       </div>
     </div>
-
-
   </div>
 </section>
 <!-- End about Us -->
+
+<!-- start WHY CHOOSE US -->
+<section class="" id="whychooseUs">
+  <div class="container p-5">
+    <h1 class="pb-2 text-primary"> WHY CHOOSE US?</h1>
+  <div class="row">
+    <div class="col-md-12 col-lg-3 pb-lg-0 pb-4">
+    <h2 class="text-md-center text-lg-start text-center ">  <i class="fa-solid fa-house-chimney"></i></h2>
+      Deal with all tire malfunctions by repairing them on site or 
+      by transporting the tires to a specialized center
+    </div>
+    <div class="col-md-12 col-lg-3 pb-lg-0 pb-4">
+    <h2 class="text-md-center text-lg-start text-center ">  <i class="fa-solid fa-house-chimney"></i></h2>
+    We charge the battery or replace it with a new battery
+    </div>
+    <div class="col-md-12 col-lg-3  pb-lg-0 pb-4">
+    <h2 class="text-md-center text-lg-start text-center ">  <i class="fa-solid fa-house-chimney"></i></h2>
+      Customers can choose the desired surface type from a list of all 
+      surface types to suit all types of vehicles.
+    </div>
+    <div class="col-md-12 col-lg-3  ">
+    <h2 class="text-md-center text-lg-start text-center ">  <i class="fa-solid fa-house-chimney"></i></h2>
+      In-town or out-of-town gasoline or diesel delivery.
+    </div>
+  </div>
+  </div>
+
+</section>
+<!-- End WHY CHOOSE US -->
 <?php require APPROOT . '/views/inc/footer.php'; ?>
