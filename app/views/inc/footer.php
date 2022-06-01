6ccb8ca0e108907d567c0dbe61a0e50b3b203624
MaititeMohamed <maititemohamed@gmail.com>
2022-05-31 21:20:13 -1200

    add footer

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,9 +1,138 @@
+<!-- start footer  -->
+<!-- Footer -->
+<footer class=" text-center text-white " style="background-color:#0083DE;">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+      <!-- Facebook -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+
+      <!-- Twitter -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+
+      <!-- Google -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+
+      <!-- Instagram -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+
+      <!-- Linkedin -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+
+      <!-- Github -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
+    </section>
+    <!-- Section: Social media -->
+
+
+
+    <!-- Section: Text -->
+    <section class="mb-4">
+
+    </section>
+    <!-- Section: Text -->
+
+    <!-- Section: Links -->
+    <section class="">
+      <!--Grid row-->
+      <div class="row">
+       
+
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Quik link</h5>
+
+          <ul class="list-unstyled mb-0 ">
+            <li>
+              <a href="#!" class="text-white text-decoration-none"> Home</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white text-decoration-none">About Us</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white text-decoration-none">Services</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white text-decoration-none">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Services</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-white text-decoration-none">All Servicerl</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white text-decoration-none">Locate A Servicer Point</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white text-decoration-none">Repairing pack</a>
+            </li>
+
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Support</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-white text-decoration-none">Contact</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white text-decoration-none">E-mail</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white text-decoration-none">Fax</a>
+            </li>
+
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" >
+    © 2020 Copyright Fixeto 2021. All Rights Reserved
+    <a class=" text-white" href="#">Fixeto</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+<!-- end footer  -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <!-- bundle link -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
- integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-  crossorigin="anonymous"></script>
-  <!-- js link  -->
-  <script src="<?php echo URLROOT; ?>/js/main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<!-- js link  -->
+<script src="<?php echo URLROOT; ?>/js/main.js"></script>
 </body>
+
 </html>
