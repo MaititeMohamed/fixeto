8f810213e9d566511aabc04d37954c184e679bce
MaititeMohamed <maititemohamed@gmail.com>
2022-06-02 20:05:14 -1200

    add pages mechaniclist

---
+++ app/views/pages/mechaniclist.php
@@ -0,0 +1,112 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<!-- start card mechanical   -->
+<div class="container-fluid " style="background-color: #eee;">
+    <div class="container p-4  ">
+        <!-- --- start card--- -->
+        
+                <div class="row d-flex  h-100 ">
+                    <div class="col-md-12 col-lg-4">
+
+                        <div class="card mb-4" style="border-radius: 15px;">
+                            <div class="card-body text-center">
+                                <div class="mt-3 mb-4">
+                                    <img src="<?php echo URLROOT; ?>/public/img/users/image_03.png" class="rounded-circle img-fluid" style="width: 100px;" />
+                                </div>
+                                <h4 class="mb-2">Moustapha Mouhamed</h4>
+                                <p class="text-muted mb-4">2KM <span class="mx-2">|</span> <span>3min</span></p>
+                                <div class="mb-4 pb-2">
+                                    <button type="button" class="btn btn-outline-primary btn-floating">
+                                        <i class="fab fa-facebook-f fa-lg"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-outline-primary btn-floating">
+                                        <i class="fab fa-twitter fa-lg"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-outline-primary btn-floating">
+                                        <i class="fab fa-instagram fa-lg"></i>
+                                    </button>
+                                </div>
+
+                                <div class="d-flex justify-content-center mb-2">
+                                <button type="button" class="btn btn-primary">  MESSAGE </button>
+                                <button type="button" class="btn btn-primary ms-2 ms-sm-1">PROFILE</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                    <!-- card 2 -->
+                    <div class="col-md-12 col-lg-4">
+
+                        <div class="card mb-4" style="border-radius: 15px;">
+                            <div class="card-body text-center">
+                                <div class="mt-3 mb-4">
+                                    <img src="<?php echo URLROOT; ?>/public/img/users/image_03.png" class="rounded-circle img-fluid" style="width: 100px;" />
+                                </div>
+                                <h4 class="mb-2">Moustapha Mouhamed</h4>
+                                <p class="text-muted mb-4">2KM <span class="mx-2">|</span> <span>3min</span></p>
+                                <div class="mb-4 pb-2">
+                                    <button type="button" class="btn btn-outline-primary btn-floating">
+                                        <i class="fab fa-facebook-f fa-lg"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-outline-primary btn-floating">
+                                        <i class="fab fa-twitter fa-lg"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-outline-primary btn-floating">
+                                    <i class="fab fa-instagram fa-lg"></i>                                    </button>
+                                </div>
+
+                                <div class="d-flex justify-content-center mb-2">
+                                <button type="button" class="btn btn-primary">  MESSAGE </button>
+                                <button type="button" class="btn btn-primary ms-2 ms-sm-1">PROFILE</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                    <!-- secand card  -->
+                    <div class="col-md-12 col-lg-4">
+
+                        <div class="card mb-4" style="border-radius: 15px;">
+                            <div class="card-body text-center">
+                                <div class="mt-3 mb-4">
+                                    <img src="<?php echo URLROOT; ?>/public/img/users/image_03.png" class="rounded-circle img-fluid" style="width: 100px;" />
+                                </div>
+                                <h4 class="mb-2">Moustapha Mouhamed</h4>
+                                <p class="text-muted mb-4">2KM <span class="mx-2">|</span> <span>3min</span></p>
+                                <div class="mb-4 pb-2">
+                                    <button type="button" class="btn btn-outline-primary btn-floating">
+                                        <i class="fab fa-facebook-f fa-lg"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-outline-primary btn-floating">
+                                        <i class="fab fa-twitter fa-lg"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-outline-primary btn-floating">
+                                    <i class="fab fa-instagram fa-lg"></i>
+                                                                    </button>
+                                </div>
+
+                                <div class="d-flex justify-content-center mb-2">
+                                <button type="button" class="btn btn-primary">  MESSAGE </button>
+                                <button type="button" class="btn btn-primary ms-2 ms-sm-1">PROFILE</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+         
+        <!-- --- end card-- -->
+        <div class="text-center">
+        <button type="button" class="btn btn-primary ">  show mor </button>
+
+        </div>
+    </div>
+</div>
+<!-- end card mechanical   -->
+<?php require APPROOT . '/views/inc/footer.php'; ?>
