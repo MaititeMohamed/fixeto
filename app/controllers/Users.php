8e99c17339ca1ee9a403615bf76aaa6ebe0469f4
MaititeMohamed <maititemohamed@gmail.com>
2022-06-05 07:09:12 -1200

    create login methode

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -5,17 +5,54 @@ public function __construct()
 {
     
 }
+    //register methode 
+    public function register(){
+        //check for POST
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            //Process form
 
-public function register(){
-    //check for POST
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        //Process form
+        }else{
+            //Init Data 
+        $data=[
+            'FirstName'=> '',
+            'LastName'=> '',
+            'Email'=> '',
+            'password'=> '',
+            'phone'=> '',
+            'confirm_password'=>'',
+            'FirstName_error'=>'',
+            'passworde_error' => '',
+            'LaststName_error'=>'',
+            'Email_error'=>'',
+            'phone_error'=>'',
+            'confirm_password_error' => ''
 
-    }else{
-        //load form
-        echo 'load form';
+        ];
+        //load view
+        $this->view('users/register',$data);
+        }
     }
-}
+       //register login 
+       public function login(){
+        //check for POST
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            //Process form
+
+        }else{
+            //Init Data 
+        $data=[
+           
+            'Email'=> '',
+            'password'=> '',
+            'passworde_error' => '',        
+            'Email_error'=>''
+
+        ];
+        //load view
+        $this->view('users/login',$data);
+        }
+    }
+
 
 
 }
