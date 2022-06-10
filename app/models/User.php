70c0463172f90a3d19a380f178affaa2991663d2
MaititeMohamed <maititemohamed@gmail.com>
2022-06-10 08:34:06 -1200

    create methode register mechanical

--- app/models/User.php
+++ app/models/User.php
@@ -1,74 +1,135 @@
 <?php
-  class User {
-    private $db;
+class User
+{
+  private $db;
 
-    public function __construct(){
-      $this->db = new Database;
-    }
-         // Add User / Register
-     public function register($data){
-        // Prepare Query
-        $this->db->query('INSERT INTO `client`( `FirstName`, `LastName`, `phone`)
-        VALUES  (:FirstName, :LastName,:phone); 
-        INSERT INTO `users`(`FirstName`,`LastName`,`Email`,`password`) 
-        VALUES(:FirstName,:LastName,:Email,:password);');
-  
-        // Bind Values
-        $this->db->bind(':FirstName', $data['FirstName']);
-        $this->db->bind(':LastName', $data['LastName']);
-        $this->db->bind(':Email', $data['Email']);
-        $this->db->bind(':password',$data['password']);
-        $this->db->bind(':phone', $data['phone']);
-        //Execute
-        if($this->db->execute()){
-          return true;
-        } else {
-          return false;
-        }
-      }
-      
+  public function __construct()
+  {
+    $this->db = new Database;
+  }
+  //  registermechanical 
+  // create function for registermechanical
+  public function registermechanical($data)
+  {
+      // Prepare Query
+      $this->db->query('INSERT INTO `mechanical`(`FirstName`,`LastName`,`City`,`phone`,`twitter`,`instagram`,`facebook`)
+    VALUES  (:FirstName,:LastName,:City,:phone,:twitter,:instagram,:facebook); 
+    INSERT INTO `users`(`FirstName`,`LastName`,`Email`,`password`)
+          VALUES(:FirstName,:LastName,:Email,:password);');
 
+      // Bind Values
+      $this->db->bind(':FirstName', $data['FirstName']);
+      $this->db->bind(':LastName', $data['LastName']);
+      $this->db->bind(':Email', $data['Email']);
+      $this->db->bind(':password', $data['password']);
+      $this->db->bind(':phone', $data['phone']);
+      $this->db->bind(':twitter', $data['twitter']);
+      $this->db->bind(':instagram', $data['instagram']);
+      $this->db->bind(':facebook', $data['facebook']);
+      $this->db->bind(':City', $data['City']);
 
-   // create function get idC client and Update fkclient in table user
-    public function selectClientId($data){
-        $this->db->query('SELECT `idC` FROM `client` WHERE `FirstName` = :FirstName AND `LastName` = :LastName AND `phone` = :phone');
-        $this->db->bind(':FirstName', $data['FirstName']);
-        $this->db->bind(':LastName', $data['LastName']);
-        $this->db->bind(':phone', $data['phone']);
-        $row = $this->db->single();
-        $this->db->query('UPDATE `users` SET `fkclient` = :fkclient WHERE `Email` = :Email');
-        $this->db->bind(':fkclient', $row->idC);
-        $this->db->bind(':Email', $data['Email']);
-        $this->db->execute();
-      }
-
-
-       // Login / Authenticate User
-    public function login($Email,$password){
-      $this->db->query("SELECT * FROM users WHERE Email = :Email");
-      $this->db->bind(':Email', $Email);
-      $row = $this->db->single();
-      $hashed_password = $row->password;
-      if(password_verify($password, $hashed_password)){
-        return $row;
+      //Execute
+      if ($this->db->execute()) {
+        return true;
       } else {
         return false;
       }
+  }
+    // create function get idC client and Update fkclient in table user
+    public function selectMechanicalId($data)
+    {
+      $this->db->query('SELECT `idm` FROM `mechanical` WHERE `FirstName` = :FirstName AND `LastName` = :LastName AND `phone` = :phone');
+      $this->db->bind(':FirstName', $data['FirstName']);
+      $this->db->bind(':LastName', $data['LastName']);
+      $this->db->bind(':phone', $data['phone']);
+      $row = $this->db->single();
+      $this->db->query('UPDATE `users` SET `fkmechanical` = :fkmechanical WHERE `Email` = :Email');
+      $this->db->bind(':fkmechanical', $row->idm);
+      $this->db->bind(':Email', $data['Email']);
+      $this->db->execute();
     }
-      // Find USer BY Email
-        public function findUserByEmail($Email){
-                $this->db->query("SELECT * FROM users WHERE Email = :Email");
-                $this->db->bind(':Email', $Email);
-                $row = $this->db->single();
-                //Check Rows
-                if($this->db->rowCount() > 0){
-                    return true;
-                }else{
-                     return false;
-                }
-        }
-     
-// part user mechanical 
+  // // Find USer BY Email
+  // public function findMechanicalByEmail($Email)
+  // {
+  //   $this->db->query("SELECT * FROM mechanical WHERE Email = :Email");
+  //   $this->db->bind(':Email', $Email);
+  //   $row = $this->db->single();
+  //   //Check Rows
+  //   if ($this->db->rowCount() > 0) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+
+
+
+  //  Registerclient
+  public function registerclient($data)
+  {
+    // Prepare Query
+    $this->db->query('INSERT INTO `client`( `FirstName`, `LastName`, `phone`)
+        VALUES  (:FirstName, :LastName,:phone); 
+        INSERT INTO `users`(`FirstName`,`LastName`,`Email`,`password`) 
+        VALUES(:FirstName,:LastName,:Email,:password);');
+
+    // Bind Values
+    $this->db->bind(':FirstName', $data['FirstName']);
+    $this->db->bind(':LastName', $data['LastName']);
+    $this->db->bind(':Email', $data['Email']);
+    $this->db->bind(':password', $data['password']);
+    $this->db->bind(':phone', $data['phone']);
+    //Execute
+    if ($this->db->execute()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // create function get idC client and Update fkclient in table user
+  public function selectClientId($data)
+  {
+    $this->db->query('SELECT `idC` FROM `client` WHERE `FirstName` = :FirstName AND `LastName` = :LastName AND `phone` = :phone');
+    $this->db->bind(':FirstName', $data['FirstName']);
+    $this->db->bind(':LastName', $data['LastName']);
+    $this->db->bind(':phone', $data['phone']);
+    $row = $this->db->single();
+    $this->db->query('UPDATE `users` SET `fkclient` = :fkclient WHERE `Email` = :Email');
+    $this->db->bind(':fkclient', $row->idC);
+    $this->db->bind(':Email', $data['Email']);
+    $this->db->execute();
+  }
+
+
+  // Login / Authenticate User
+  public function login($Email, $password)
+  {
+    $this->db->query("SELECT * FROM users WHERE Email = :Email");
+    $this->db->bind(':Email', $Email);
+    $row = $this->db->single();
+    $hashed_password = $row->password;
+    if (password_verify($password, $hashed_password)) {
+      return $row;
+    } else {
+      return false;
+    }
+  }
+  // Find USer BY Email
+  public function findUserByEmail($Email)
+  {
+    $this->db->query("SELECT * FROM users WHERE Email = :Email");
+    $this->db->bind(':Email', $Email);
+    $row = $this->db->single();
+    //Check Rows
+    if ($this->db->rowCount() > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // part user mechanical 
 
 
 
@@ -77,11 +138,11 @@
 
 
 
-       
 
 
 
-    
+
+
 
 
 
