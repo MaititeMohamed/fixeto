efae9a192ed43dc3b4bfbf7ea5858ca6c8706fda
MaititeMohamed <maititemohamed@gmail.com>
2022-06-10 08:34:51 -1200

    create profile view for client

--- app/views/clients/index.php
+++ app/views/clients/index.php
@@ -1,3 +1,54 @@
-client view
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<section class="" style="background-color: #f4f5f7;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-lg-6 mb-4 mb-lg-0">
+        <div class="card mb-3" style="border-radius: .5rem;">
+          <div class="row g-0">
+            <div class="col-md-4 gradient-custom text-center text-white bg-primary"
+              style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem; ">
+              <img src="<?php echo URLROOT; ?>/public/img/users/image_03.png" class="rounded-circle img-fluid mt-5" style="width: 100px;" alt="avatar" />
+
+              <h5>Marie Horwitz</h5>
+              <p>Web Designer</p>
+              
+            </div>
+            <div class="col-md-8">
+              <div class="card-body p-4">
+                <h6>Information</h6>
+                <hr class="mt-0 mb-4">
+                <div class="row pt-1">
+                  <div class="col-6 mb-3">
+                    <h6>Email</h6>
+                    <p class="text-muted">info@example.com</p>
+                  </div>
+                  <div class="col-6 mb-3">
+                    <h6>Phone</h6>
+                    <p class="text-muted">123 456 789</p>
+                  </div>
+                </div>
+                <h6>Projects</h6>
+                <hr class="mt-0 mb-4">
+                <div class="row pt-1">
+                  <div class="col-6 mb-3">
+                    <h6>Recent</h6>
+                    <p class="text-muted">Lorem ipsum</p>
+                  </div>
+                  <div class="col-6 mb-3">
+                    <h6>Most Viewed</h6>
+                    <p class="text-muted">Dolor sit amet</p>
+                  </div>
+                </div>
+               
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
 
 
