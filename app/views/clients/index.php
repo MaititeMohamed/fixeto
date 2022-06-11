bb5aac478188b2cce75dd78c76dc01381084f0ec
MaititeMohamed <maititemohamed@gmail.com>
2022-06-11 02:53:11 -1200

    add info to profile client

--- app/views/clients/index.php
+++ app/views/clients/index.php
@@ -1,5 +1,5 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-
+<?php foreach ($data['client'] as $client) : ?>
 <section class="" style="background-color: #f4f5f7;">
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
@@ -8,10 +8,10 @@
           <div class="row g-0">
             <div class="col-md-4 gradient-custom text-center text-white bg-primary"
               style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem; ">
-              <img src="<?php echo URLROOT; ?>/public/img/users/image_03.png" class="rounded-circle img-fluid mt-5" style="width: 100px;" alt="avatar" />
+              <img src="<?php echo URLROOT; ?>/public/img/users/image_03.png" class="rounded-circle img-fluid mt-4" style="width: 100px;" alt="avatar" />
 
-              <h5>Marie Horwitz</h5>
-              <p>Web Designer</p>
+              <h5><?php echo $client->FirstName;?>&nbsp;<?php echo $client->LastName;?></h5>
+              <p>Client</p>
               
             </div>
             <div class="col-md-8">
@@ -20,24 +20,25 @@
                 <hr class="mt-0 mb-4">
                 <div class="row pt-1">
                   <div class="col-6 mb-3">
+                    
                     <h6>Email</h6>
-                    <p class="text-muted">info@example.com</p>
-                  </div>
+                    <p class="text-muted"><?php echo $client->Email;?></p>
+                     
+                </div>
                   <div class="col-6 mb-3">
                     <h6>Phone</h6>
-                    <p class="text-muted">123 456 789</p>
+                    <p class="text-muted"><?= $client->phone;?></p>
                   </div>
                 </div>
-                <h6>Projects</h6>
+                <h6>Manage your acount</h6>
                 <hr class="mt-0 mb-4">
                 <div class="row pt-1">
-                  <div class="col-6 mb-3">
-                    <h6>Recent</h6>
-                    <p class="text-muted">Lorem ipsum</p>
+                  <div class="col-6 mb-3"> 
+                  <button type="button" class="btn btn-danger">Delete Acount</button>
+
                   </div>
-                  <div class="col-6 mb-3">
-                    <h6>Most Viewed</h6>
-                    <p class="text-muted">Dolor sit amet</p>
+                   <div class="col-6 mb-3">
+                    <button type="button" class="btn btn-success">Update Acount</button>
                   </div>
                 </div>
                
@@ -49,6 +50,8 @@
     </div>
   </div>
 </section>
+<?php endforeach; ?>
+
 <?php require APPROOT . '/views/inc/footer.php'; ?>
 
 
