d11c74fecd4358a08e4287298e0cca8f12260186
MaititeMohamed <maititemohamed@gmail.com>
2022-06-11 02:53:51 -1200

    show nao of user loged in the navbar

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -31,11 +31,13 @@
         <!-- stat dropdown  -->
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+           <?=$_SESSION['user_name'];?>
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <!-- <li><a class="dropdown-item" href="#">Action</a></li> -->
             <li><a class="dropdown-item" href="<?php echo URLROOT; ?>/clients/index">My profile</a></li>
+            <li><a class="dropdown-item" href="<?php echo URLROOT; ?>/pages/mechaniclist">find mechanical</a></li>
+
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="<?php echo URLROOT; ?>/users/logout">Logout</a></li>
           </ul>
