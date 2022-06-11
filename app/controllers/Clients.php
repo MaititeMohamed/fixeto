aed0114eb4c432f64c77367e7e01a011e63c7706
MaititeMohamed <maititemohamed@gmail.com>
2022-06-11 02:50:31 -1200

    select information client for show info in profile client

--- app/controllers/Clients.php
+++ app/controllers/Clients.php
@@ -1,11 +1,26 @@
 <?php
 
 class Clients extends Controller {
+    public function __construct(){
+        if(!isset($_SESSION['iduser'])){
+          redirect('users/login');
+        }
+       //load model client
+         $this->clientModel = $this->model('Client');
+      }
 
 public function index()
-{
-
-    $data =[];
+{ 
+    //git client
+    $clients = $this->clientModel->getClientInfo();
+    
+//    echo '<pre>';
+//     var_dump($clients);
+//     echo '</pre>';
+//   exit();
+    $data =[
+        'client' => $clients,
+    ];
 
     $this->view('clients/index',$data);
 }
