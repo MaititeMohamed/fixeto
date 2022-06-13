0b189c14dba957c1b1d71a96c6b235884e85f978
MaititeMohamed <maititemohamed@gmail.com>
2022-06-12 12:26:08 -1200

    cleane code

--- app/views/clients/index.php
+++ app/views/clients/index.php
@@ -5,13 +5,12 @@
       <div class="col col-lg-6 mb-4 mb-lg-0">
         <div class="card mb-3" style="border-radius: .5rem;">
           <div class="row g-0">
-            <div class="col-md-4 gradient-custom text-center text-white bg-primary"
-              style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem; ">
+            <div class="col-md-4 gradient-custom text-center text-white bg-primary" style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem; ">
               <img src="<?php echo URLROOT; ?>/public/img/users/image_03.png" class="rounded-circle img-fluid mt-4" style="width: 100px;" alt="avatar" />
 
-              <h5><?php echo $data['client']->FirstName;?>&nbsp;<?php echo  $data['client']->LastName?></h5>
+              <h5><?php echo $data['client']->FirstName; ?>&nbsp;<?php echo  $data['client']->LastName ?></h5>
               <p>Client</p>
-              
+
             </div>
             <div class="col-md-8">
               <div class="card-body p-4">
@@ -19,31 +18,30 @@
                 <hr class="mt-0 mb-4">
                 <div class="row pt-1">
                   <div class="col-6 mb-3">
-                    
+
                     <h6>Email</h6>
-                    <p class="text-muted"><?php echo $data['client']->Email;?></p>
-                     
-                </div>
+                    <p class="text-muted"><?php echo $data['client']->Email; ?></p>
+
+                  </div>
                   <div class="col-6 mb-3">
                     <h6>Phone</h6>
-                    <p class="text-muted"><?= $data['client']->phone;?></p>
+                    <p class="text-muted"><?= $data['client']->phone; ?></p>
                   </div>
                 </div>
                 <h6>Manage your acount</h6>
                 <hr class="mt-0 mb-4">
                 <div class="row pt-1">
-                  <div class="col-6 mb-3"> 
-                  
-                  <form class="pull-right" action="<?php echo URLROOT; ?>/Clients/deleteClient/<?= $data['client']->idC;?>" method="post">
-                    <input type="submit" class="btn btn-danger" value="Delete Acount">
-                  </form>
+                  <div class="col-6 mb-3">
+                    <form class="pull-right" action="<?php echo URLROOT; ?>/Clients/deleteClient/<?= $data['client']->idC; ?>" method="post">
+                      <input type="submit" class="btn btn-danger" value="Delete Acount">
+                    </form>
                   </div>
-                   <div class="col-6 mb-3"> 
-                  <a class="btn btn-dark" href="<?php echo URLROOT; ?>/Clients/EditClient/<?= $data['client']->idC;?>">Update Acount</a>
-            
-                    </div>
+                  <div class="col-6 mb-3">
+                    <a class="btn btn-dark" href="<?php echo URLROOT; ?>/Clients/EditClient/<?= $data['client']->idC; ?>">Update Acount</a>
+
+                  </div>
                 </div>
-               
+
               </div>
             </div>
           </div>
@@ -55,5 +53,3 @@
 
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
-
-
