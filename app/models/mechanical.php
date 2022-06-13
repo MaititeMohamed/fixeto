879954bb3511cb51efcf699436f45cfc9b97ff09
MaititeMohamed <maititemohamed@gmail.com>
2022-06-12 12:25:25 -1200

    create update methode in the modle

--- app/models/mechanical.php
+++ app/models/mechanical.php
@@ -15,4 +15,32 @@ class mechanical extends Controller
         $mechanicalinfo = $this->db->single();
         return $mechanicalinfo;
     }
+
+    // Edit mechanical
+    public function Edit($data)
+    {
+        // Prepare Query
+
+        $this->db->query('UPDATE `mechanical` SET `FirstName` = :FirstName, `LastName` = :LastName,`City` = :City,`phone` = :phone,`twitter` = :twitter,`instagram` = :instagram,`facebook` = :facebook WHERE `idm` = :idm;
+      UPDATE `users` SET `FirstName` = :FirstName, `LastName` = :LastName, `Email` = :Email, `password` = :password WHERE `iduser` = :iduser');
+
+        // Bind Values
+        $this->db->bind(':FirstName', $data['FirstName']);
+        $this->db->bind(':LastName', $data['LastName']);
+        $this->db->bind(':City', $data['City']);
+        $this->db->bind(':phone', $data['phone']);
+        $this->db->bind(':twitter', $data['twitter']);
+        $this->db->bind(':instagram', $data['instagram']);
+        $this->db->bind(':facebook', $data['facebook']);
+        $this->db->bind(':idm', $data['idm']);
+        $this->db->bind(':Email', $data['Email']);
+        $this->db->bind(':password', $data['password']);
+        $this->db->bind(':iduser', $_SESSION['iduser']);
+        //Execute
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
