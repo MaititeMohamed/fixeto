6d1a5f763046c4c50687c50a6e6b4755e6462af3
MaititeMohamed <maititemohamed@gmail.com>
2022-06-14 00:17:44 -1200

    create methode conttroler for  get inactive mechanical

--- app/controllers/Dashbords.php
+++ app/controllers/Dashbords.php
@@ -1,19 +1,49 @@
 <?php
-  class Dashbords extends Controller {
-    public function __construct(){
-     
+class Dashbords extends Controller
+{
+  public function __construct()
+  {
+    if (!isset($_SESSION['iduser'])) {
+      redirect('users/login');
     }
-    
- 
+    //load Dashboard model
+    $this->dashboardModel = $this->model('Dashboard');
 
-    
-    // dashboard
-    public function dashboard(){
-      $data = [
-        'title' => 'dashboard '
-      ];
-
-      $this->view('dashboards/dashboard', $data);
-    }
-
+    // load model user we gan a use it for destroy session
+    $this->userModel = $this->model('User');
   }
+
+
+
+  // dashboard
+  public function dashboard()
+  {
+    //git client
+    $NumberofUsers = $this->dashboardModel-> getNumberOfUsers();
+    $NumberofClients = $this->dashboardModel-> getNumberOfClients();
+    $NumberofMechanicals = $this->dashboardModel-> getNumberOfMechanicals();
+    $InActivemechanicalInfo = $this->dashboardModel-> getInActivemechanicalInfo();
+    
+      //  echo '<pre>';
+      //   var_dump($InActivemechanicalInfo);
+      //   echo '</pre>';
+      // exit();
+    $data = [
+      'Numberofmechanicals' =>$NumberofMechanicals,
+      'NumberofClients' => $NumberofClients,
+      'NumberofUsers' => $NumberofUsers,
+      'InActivemechanicalInfo' => $InActivemechanicalInfo
+    ];
+
+
+    $this->view('dashboards/dashboard', $data);
+  }
+
+  // create methode setActive
+public function setActive($idm)
+{
+  $this->dashboardModel->setActive($idm);
+  
+  redirect('dashboards/dashboard');
+}
+}
