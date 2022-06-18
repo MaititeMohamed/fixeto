8ec2cda11da3c9e6bf5bf10ff4abad1d01cd96a8
MaititeMohamed <maititemohamed@gmail.com>
2022-06-18 09:25:02 -1200

    edit nav

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg ">
   <div class="container-fluid">
     <a class="navbar-brand " href="#">
-      <img src="<?php echo URLROOT; ?>/public/img/logo/fixeto.png" />
+      <img src="<?php echo URLROOT; ?>/public/img/logo/fixeto.png " alt="img" />
     </a>
     <button class="navbar-toggler me-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <i class="fa-solid fa-bars"></i>
@@ -24,47 +24,49 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Contact</a>
         </li>
-        <?php if (isset($_SESSION['iduser'])) : ?>
+                <?php if (isset($_SESSION['iduser'])) : ?>
+                  <!-- stat dropdown  -->
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      <?php echo $_SESSION['user_name']; ?>
+                    </a>
+                    <?php if ($_SESSION['client'] != null || $_SESSION['mechanical']!= null) : ?>
+                      <?php if ($_SESSION['mechanical'] != null) : ?>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <li><a class="dropdown-item" href="<?php echo URLROOT; ?>/mechanicals/mechanicalprofile">My profile</a></li>
+                            <?php else : ?>
+                              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="<?php echo URLROOT; ?>/clients/index">My profile</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="<?php echo URLROOT; ?>/mechanicals/mechaniclist">find mechanical</a> </li>
+                              <?php endif; ?>
+                              <li>
+                                <hr class="dropdown-divider">
+                              </li>
+                              <li><a class="dropdown-item" href="<?php echo URLROOT; ?>/users/logout">Logout</a></li>
+                              </ul>
+                              </li>
+                             <?php else :?>  
+                              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <li><a class="dropdown-item" href="<?php echo URLROOT; ?>/Dashbords/dashboard">dashboard</a></li>
+                              <li>
+                                <hr class="dropdown-divider">
+                              </li>
+                              <li><a class="dropdown-item" href="<?php echo URLROOT; ?>/users/logout">Logout</a></li>
+                              </ul>
+                            <?php endif; ?> 
+                          
+                            <!-- end dropdown  -->
 
-
-          <!-- stat dropdown  -->
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              <?php echo $_SESSION['user_name']; ?>
-            </a>
-            <?php if ($_SESSION['client'] != null || $_SESSION['mechanical'] != null) : ?>
-              <?php if ($_SESSION['mechanical'] != null) : ?>
-
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="<?php echo URLROOT; ?>/mechanicals/mechanicalprofile">My profile</a></li>
-                <?php else : ?>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="<?php echo URLROOT; ?>/clients/index">My profile</a></li>
-
-                    <li>
-                      <hr class="dropdown-divider">
-                    </li>
-                    <li><a class="dropdown-item" href="<?php echo URLROOT; ?>/mechanicals/mechaniclist">find mechanical</a></li>
-
-                  <?php endif; ?>
+                            <?php else : ?>
+                             <li class="nav-item">
+                              <a class="nav-link me-2" href="<?php echo URLROOT; ?>/users/login">login</a>
+                             </li>
+                   <?php endif; ?>
                   
-                  <li>
-                    <hr class="dropdown-divider">
-                  </li>
-                  <li><a class="dropdown-item" href="<?php echo URLROOT; ?>/users/logout">Logout</a></li>
-                  </ul>
-                </li>
-                </ul>
-                <?php endif; ?>
               
-                <!-- end dropdown  -->
-              <?php else : ?>
-                <li class="nav-item">
-                  <a class="nav-link me-2" href="<?php echo URLROOT; ?>/users/login">login</a>
-                </li>
 
-              <?php endif; ?>
-      </ul>
+  </ul>
 
     </div>
   </div>
