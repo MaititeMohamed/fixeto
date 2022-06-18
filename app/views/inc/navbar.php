379fe55087ec8fbf26a95c2d8148a1493a21f968
MaititeMohamed <maititemohamed@gmail.com>
2022-06-18 07:49:00 -1200

    add links section

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -3,7 +3,7 @@
     <a class="navbar-brand " href="#">
       <img src="<?php echo URLROOT; ?>/public/img/logo/fixeto.png" />
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler me-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <i class="fa-solid fa-bars"></i>
     </button>
     <div class="collapse navbar-collapse me-3" id="navbarSupportedContent">
@@ -14,11 +14,11 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link" href="#AboutUs">About</a>
+          <a class="nav-link" href="<?php echo URLROOT; ?>/#AboutUs">About</a>
         </li>
 
         <li class="nav-item">
-          <a class="nav-link" href="#OURSERVICES">Services</a>
+          <a class="nav-link" href="<?php echo URLROOT; ?>/#OURSERVICES">Services</a>
         </li>
 
         <li class="nav-item">
