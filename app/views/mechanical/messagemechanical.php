0de0c36189a5859df997b0fceba8ae1ffb05246e
MaititeMohamed <maititemohamed@gmail.com>
2022-06-17 23:05:16 -1200

    style card message

--- app/views/mechanical/messagemechanical.php
+++ app/views/mechanical/messagemechanical.php
@@ -1,24 +1,27 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
 <!-- start message for mechanical  -->
-<div class="container  d-flex ">
-<?php foreach ($data['messageInfo'] as $message) : ?>
+<div class="container  ">
 
-<div class="card mb-3 ms-5 flex-fill" style="max-width:540px;">
-  <div class="row g-0">
-    
-    <div class="col-md-8">
+
+
+
+
+
+<div class="row">
+<?php foreach ($data['messageInfo'] as $message) : ?>
+  <div class="col-sm-6 mb-3 me-2 ">
+    <div class="card">
       <div class="card-body">
         <h5 class="card-title">your message</h5>
         <p> from :<?= $message->FirstName ?></p>
         <p class="card-text"> message :<?= $message->content ?>.</p>
-        <p class="card-text"><small class="text-muted"><p> in :<?= $message->datemessage ?></p></small></p>
+        <p class="card-text">in :<?= $message->datemessage ?></p>
       </div>
     </div>
   </div>
+  <?php endforeach; ?>
 </div>
-
-<?php endforeach; ?>
 </div>
 <!-- end message for mechanical  -->
 
