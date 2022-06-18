898709e2d755f76dda66c282aa692afae440146d
MaititeMohamed <maititemohamed@gmail.com>
2022-06-18 01:26:45 -1200

    add view for shwo feedbck info

---
+++ app/views/mechanical/feebackinfomechanical.php
@@ -0,0 +1,25 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<!-- start feedbacl for mechanical  -->
+<div class="container  ">
+<div class="row">
+<?php foreach ($data['feedbackInfo'] as $feed) : ?>
+  <div class="col-sm-6 mb-3 me-2 ">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">your feedback</h5>
+        <p> from :<?= $feed->FirstName ?></p>
+        <p class="card-text"> feed :<?= $feed->content ?>.</p>
+        <p class="card-text">in :<?= $feed->datefeedback ?></p>
+      </div>
+    </div>
+  </div>
+  <?php endforeach; ?>
+</div>
+</div>
+<!-- end feedback for mechanical  -->
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
+
+                                                                   
+                                               
