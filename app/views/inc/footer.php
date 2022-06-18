3bf5d800c28e918c247e956ae79ff3878b934f64
MaititeMohamed <maititemohamed@gmail.com>
2022-06-18 04:19:38 -1200

    reomve padding top

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,6 +1,6 @@
 <!-- start footer  -->
 <!-- Footer -->
-<footer class=" text-center text-white mb-0  " style="background-color:#0083DE; margin-top:61px">
+<footer class=" text-center text-white mb-0  " style="background-color:#0083DE; ">
   <!-- Grid container -->
   <div class="container p-4">
     <!-- Section: Social media -->
